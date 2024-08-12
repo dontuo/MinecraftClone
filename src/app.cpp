e83cf2afcf0c06fdb5619875e602d8693d31ec98
dontuo <bench.oleg@gmail.com>
2024-08-13 00:02:23 +0300

    implement window class in class App

--- src/app.cpp
+++ src/app.cpp
@@ -1,74 +1,41 @@
 #include "app.h"
 
-#include <iostream>
-#include <stdexcept>
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <learnopengl/camera.h>
 
 App::App(){}
 
-void App::GLFW_FramebufferSizeCallback(GLFWwindow *window, int width, int height)
+/*void App::GLFW_FramebufferSizeCallback(GLFWwindow *window, int width, int height)
 {
     glViewport(0,0,width,height);
-}
+}*/
 
 
-int App::Init(int width, int height,const char *name)
+void App::Init(int width, int height,const char *name)
 {
-    if(!glfwInit())
-        throw std::runtime_error("failed to create window =/ Not lucky today =/");
-    
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    mWindow = glfwCreateWindow(width, height, name,NULL, NULL);
-
-    if(!mWindow)
-        throw std::runtime_error("failed to create window =/ Not lucky today =/");
-
-    glfwMakeContextCurrent(mWindow);
-    
-    if(!gladLoadGLLoader((GLADloadproc)(glfwGetProcAddress)))
-        throw std::runtime_error("failed to load opengl =/ Not lucky today =/");
-    
-    glfwGetFramebufferSize(mWindow, &width, &height);
-
-
-    glViewport(0,0,width,height);
-
-    glfwSetFramebufferSizeCallback(mWindow, GLFW_FramebufferSizeCallback);
-    return 0;
+    mWindow.Init(width, height, name);
     
 }
 
 int App::Run()
 {
-    while(!glfwWindowShouldClose(mWindow))
+    while(!glfwWindowShouldClose(mWindow.mWindow))
     {
-        InputHandler();        
-        glClearColor(0.2,0.5,0.7, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT);    
+        InputHandler();
+        mWindow.Clear(0.2,0.5,0.7,1.0);
         
-
-        glfwSwapBuffers(mWindow);
-        glfwPollEvents();
+        mWindow.Update();
     }
     return 0;
 }
 
 App::~App()
 {
-    glfwDestroyWindow(mWindow);
+    glfwDestroyWindow(mWindow.mWindow);
     glfwTerminate();
 }
 
 void App::InputHandler()
 {
-    if(glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(mWindow, true);
+    if(glfwGetKey(mWindow.mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(mWindow.mWindow, true);
 }
 
--- src/app.h
+++ src/app.h
@@ -2,23 +2,28 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <stdexcept>
+
+#include <learnopengl/camera.h>
+#include "window.h"
 
 class App
 {
 public:
 
     App();
+
     ~App();
     
-    int Init(int width, int height,const char *name);
+    void Init(int width, int height, const char *name);
 
     int Run();
 
 private:
-    GLFWwindow *mWindow;
+    Window mWindow;
     void InputHandler();
 
-    static void GLFW_FramebufferSizeCallback(GLFWwindow *window, int width, int height);
 };
 
 
